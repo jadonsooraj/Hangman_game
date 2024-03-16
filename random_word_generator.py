1866559a438cbfbaaff1f6b41a71fd7a7dffc4b7
Sooraj Singh Jadon <jadonsuraj49@gmail.com>
2024-03-16 20:04:27 +0530

    list

--- random_word_generator.py
+++ random_word_generator.py
@@ -2,7 +2,7 @@ from random import randint
 
 def pick_random_word():
     #list of words
-    word_list=["Hulk"]
+    word_list=["hulk"]
 
     selected_index=randint(0,len(word_list)-1) #word is selected from the list
     return word_list[selected_index]
